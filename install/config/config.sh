d4f32b889acc2e1a2e54574d4524daa5dc532abe
0x3133 <136857440+0x3133@users.noreply.github.com>
2025-08-09 00:09:00 +0300

    Configure git

--- install/config/config.sh
+++ install/config/config.sh
@@ -32,16 +32,7 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global pull.rebase true
-git config --global init.defaultBranch master
-
-# Set identification from install inputs
-if [[ -n "${OMARCHY_USER_NAME//[[:space:]]/}" ]]; then
-  git config --global user.name "$OMARCHY_USER_NAME"
-fi
-
-if [[ -n "${OMARCHY_USER_EMAIL//[[:space:]]/}" ]]; then
-  git config --global user.email "$OMARCHY_USER_EMAIL"
-fi
+git config --global init.defaultBranch main
 
 # Set default XCompose that is triggered with CapsLock
 tee ~/.XCompose >/dev/null <<EOF
